6551765a0f9f09d963d2ed986d4f1df1c7d12ef6
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-01-04 12:49:32 +0530

    Time: 266 ms (70.15%), Space: 103.7 MB (72.00%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    int minimumRounds(vector<int>& tasks) {
-        unordered_map<int,int> mp;
-        int n=tasks.size();
-        set<int> s;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            mp[tasks[i]]++;
-            s.insert(tasks[i]);
+   int minimumRounds(vector<int>& A) {
+        unordered_map<int, int> count;
+        int res = 0, freq1;
+        for (int a: A)
+            ++count[a];
+        for (auto& it: count) {
+            if (it.second == 1) return -1;
+            res += (it.second + 2) / 3;
         }
-        set<int> ::iterator itr;
-     for (itr = s.begin(); itr != s.end(); itr++) {
-        if(mp[*itr]<2) return -1;
-        ans+=(mp[*itr]/3);
-         if(mp[*itr]%3!=0) ans++;
-     }
-        return ans;
+        return res;
     }
 };
