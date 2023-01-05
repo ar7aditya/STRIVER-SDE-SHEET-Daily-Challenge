0fe388fd825a368e318d75ba6ba8f669c65d9e79
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-01-05 13:04:21 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (67.40%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int BASE=1e6;
+    int RabinKarp(string source,string target){
+        int m = target.size();
+        int power = 1;
+        for(int i = 0;i<m;i++){
+            power = (power*31)%BASE;
+        }
+        int targetCode = 0;
+        for(int i = 0;i<m;i++){
+            targetCode = (targetCode*31+target[i])%BASE;
+        }
+        int hashCode = 0;
+        for(int i = 0;i<source.size();i++){
+            hashCode = (hashCode*31 + source[i])%BASE;
+            if(i<m-1) continue;
+            if(i>=m){
+                hashCode = (hashCode-source[i-m]*power)%BASE;
+            }
+            if(hashCode<0)
+                hashCode+=BASE;
+            if(hashCode == targetCode){
+                if(source.substr(i-m+1,m) == target)
+                    return i-m+1;
+            }
+        }
+        return -1;
+    }
+    
+    int strStr(string haystack, string needle) {
+        int n=haystack.size();
+        int m=needle.size();
+        if(m>n) return -1;
+        return RabinKarp(haystack,needle);
+    }
+};
