af45a6db45947d2a3921cbc4e1cebd44982280e2
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-01-05 15:32:40 +0530

    Time: 3 ms (50.40%), Space: 6.4 MB (11.56%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    int BASE=1e6;
-    int RabinKarp(string source,string target){
-        int m = target.size();
-        int power = 1;
-        for(int i = 0;i<m;i++){
-            power = (power*31)%BASE;
+    int strStr(string haystack, string needle) {
+        int m = haystack.size(), n = needle.size();
+        if (!n) {
+            return 0;
         }
-        int targetCode = 0;
-        for(int i = 0;i<m;i++){
-            targetCode = (targetCode*31+target[i])%BASE;
-        }
-        int hashCode = 0;
-        for(int i = 0;i<source.size();i++){
-            hashCode = (hashCode*31 + source[i])%BASE;
-            if(i<m-1) continue;
-            if(i>=m){
-                hashCode = (hashCode-source[i-m]*power)%BASE;
+        vector<int> lps = kmpProcess(needle);
+        for (int i = 0, j = 0; i < m;) {
+            if (haystack[i] == needle[j]) { 
+                i++, j++;
             }
-            if(hashCode<0)
-                hashCode+=BASE;
-            if(hashCode == targetCode){
-                if(source.substr(i-m+1,m) == target)
-                    return i-m+1;
+            if (j == n) {
+                return i - j;
+            }
+            if (i < m && haystack[i] != needle[j]) {
+                j ? j = lps[j - 1] : i++;
             }
         }
         return -1;
     }
-    
-    int strStr(string haystack, string needle) {
-        int n=haystack.size();
-        int m=needle.size();
-        if(m>n) return -1;
-        return RabinKarp(haystack,needle);
+private:
+    vector<int> kmpProcess(string needle) {
+        int n = needle.size();
+        vector<int> lps(n, 0);
+        for (int i = 1, len = 0; i < n;) {
+            if (needle[i] == needle[len]) {
+                lps[i++] = ++len;
+            } else if (len) {
+                len = lps[len - 1];
+            } else {
+                lps[i++] = 0;
+            }
+        }
+        return lps;
     }
 };
