d9619f53c7c5086ea9c2320cdccee4e94f57523b
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-01-10 11:00:00 +0530

    Time: 6 ms (21.18%), Space: 8.3 MB (88.59%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* cur) {
+         vector < int > postOrder;
+  if (cur == NULL) return postOrder;
+
+  stack <TreeNode * > st;
+  while (cur != NULL || !st.empty()) {
+
+    if (cur != NULL) {
+      st.push(cur);
+      cur = cur -> left;
+    } else {
+      TreeNode * temp = st.top() -> right;
+      if (temp == NULL) {
+        temp = st.top();
+        st.pop();
+        postOrder.push_back(temp -> val);
+        while (!st.empty() && temp == st.top() -> right) {
+          temp = st.top();
+          st.pop();
+          postOrder.push_back(temp -> val);
+        }
+      } else cur = temp;
+    }
+  }
+  return postOrder;
+
+}
+};
