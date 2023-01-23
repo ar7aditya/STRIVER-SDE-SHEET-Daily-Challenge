e685a6eff29c8f44252089bf40e38c56184350b2
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-01-23 17:23:25 +0530

    Time: 18 ms (95.11%), Space: 14.3 MB (37.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
+   bool iscycle(vector<int> adj[],vector<int> &vis,int id,vector<int>& dfs_vis){
+       vis[id]=1;
+       dfs_vis[id]=1;
+       for(int i:adj[id]){
+           if(dfs_vis[i]!=0) return true;
+           else {
+               if(vis[i]==0){
+                if(iscycle(adj,vis,i,dfs_vis)) return true;
+           }
+           }
+       }
+       dfs_vis[id]=0;
+       return false;
+    }
     bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<vector<int>> adj(n);
-        vector<int> indegree(n,0);
-        int a,b;
-        for(int i=0;i<pre.size();i++){
-            a=pre[i][0];
-            b=pre[i][1];
-            adj[b].push_back(a);
-            indegree[a]++;     
-        }
-        queue<int> q;
-        int k=0;
+        vector<int> adj[n];
+        for(auto edge : pre)
+            adj[edge[1]].push_back(edge[0]);
+        vector<int> per_vis(n,0);
+         vector<int> dfs_vis(n,0);
+        
         for(int i=0;i<n;i++){
-            if(indegree[i]==0) {q.push(i);k++;}
-        }
-        while(!q.empty()){
-            int node=q.front();
-            q.pop();
-            for(int i:adj[node]){
-                indegree[i]--;
-                if(indegree[i]==0) {q.push(i);k++;}
+            if(per_vis[i]==0){
+            if(iscycle(adj,per_vis,i,dfs_vis))
+                return false;
             }
         }
-        return k==n;
+        return true;
     }
+
 };
