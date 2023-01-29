05cba36d7ae00f22df74d286791670eb6a61fddc
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-01-29 20:04:13 +0530

    Time: 80 ms (14.55%), Space: 80.4 MB (14.20%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -4,16 +4,13 @@ public:
          if(i<0 ) return j+1;
          if(j<0 ) return i+1;
         if(dp[i][j]!=-1) return dp[i][j];
+         if(s1[i]==s2[j]) {
+              return dp[i][j] = f(i-1,j-1,s1,s2,dp);
+        }
         int insert = 1+f(i,j-1,s1,s2,dp);
         int delet  = 1+f(i-1,j,s1,s2,dp);
-        int norm_rep;
-        if(s1[i]==s2[j]) {
-              norm_rep   = 0+f(i-1,j-1,s1,s2,dp);
-        }
-        else{
-              norm_rep =  1+f(i-1,j-1,s1,s2,dp);
-        }
-        return dp[i][j] = min(insert,min(delet,norm_rep));
+        int replace =  1+f(i-1,j-1,s1,s2,dp);
+        return dp[i][j] = min(insert,min(delet,replace));
     }
     int minDistance(string word1, string word2) {
         int n=word1.size();
