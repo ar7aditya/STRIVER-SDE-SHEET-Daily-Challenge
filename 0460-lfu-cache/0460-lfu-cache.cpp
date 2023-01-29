2ed937c4cded961331a041462f826d0f44c82b00
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-01-29 23:06:37 +0530

    Time: 638 ms (41.52%), Space: 205.7 MB (10.86%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.cpp
+++ 0460-lfu-cache/0460-lfu-cache.cpp
@@ -1,77 +1,110 @@
-using Key_t = int;
-using Count_t = int;
-
-
-struct Node
-{
-    int value;
-    list<Key_t>::iterator itr;
+struct Node {
+    int key, value, cnt;
+    Node *next; 
+    Node *prev;
+    Node(int _key, int _value) {
+        key = _key;
+        value = _value; 
+        cnt = 1; 
+    }
+}; 
+struct List {
+    int size; 
+    Node *head; 
+    Node *tail; 
+    List() {
+        head = new Node(0, 0); 
+        tail = new Node(0,0); 
+        head->next = tail;
+        tail->prev = head; 
+        size = 0;
+    }
+    
+    void addFront(Node *node) {
+        Node* temp = head->next;
+        node->next = temp;
+        node->prev = head;
+        head->next = node;
+        temp->prev = node;
+        size++; 
+    }
+    
+    void removeNode(Node* delnode) {
+        Node* delprev = delnode->prev;
+        Node* delnext = delnode->next;
+        delprev->next = delnext;
+        delnext->prev = delprev;
+        size--; 
+    }
+    
+    
+    
 };
-
-class LFUCache
-{
-    unordered_map<Key_t, Node> m_values;
-    unordered_map<Key_t, Count_t> m_counts;
-    unordered_map<Count_t, list<Key_t>> m_countKeyMap;
-    int m_lowestFrequency;
-    int m_maxCapacity;
-
+class LFUCache {
+    map<int, Node*> keyNode; 
+    map<int, List*> freqListMap; 
+    int maxSizeCache;
+    int minFreq; 
+    int curSize; 
 public:
-    LFUCache(int capacity)
-    {
-        m_maxCapacity = capacity;
-        m_lowestFrequency = 0;
+    LFUCache(int capacity) {
+        maxSizeCache = capacity; 
+        minFreq = 0;
+        curSize = 0; 
     }
-
-    int get(int key)
-    {
-        if (m_values.find(key) == m_values.end() || m_maxCapacity <= 0)
-        {
-            return -1;
+    void updateFreqListMap(Node *node) {
+        keyNode.erase(node->key); 
+        freqListMap[node->cnt]->removeNode(node); 
+        if(node->cnt == minFreq && freqListMap[node->cnt]->size == 0) {
+            minFreq++; 
         }
-        put(key, m_values[key].value);
-        return m_values[key].value;
+        
+        List* nextHigherFreqList = new List();
+        if(freqListMap.find(node->cnt + 1) != freqListMap.end()) {
+            nextHigherFreqList = freqListMap[node->cnt + 1];
+        } 
+        node->cnt += 1; 
+        nextHigherFreqList->addFront(node); 
+        freqListMap[node->cnt] = nextHigherFreqList; 
+        keyNode[node->key] = node;
     }
-
-    void put(int key, int value)
-    {
-        if (m_maxCapacity <= 0)
-        {
+    
+    int get(int key) {
+        if(keyNode.find(key) != keyNode.end()) {
+            Node* node = keyNode[key]; 
+            int val = node->value; 
+            updateFreqListMap(node); 
+            return val; 
+        }
+        return -1; 
+    }
+    
+    void put(int key, int value) {
+        if (maxSizeCache == 0) {
             return;
         }
-
-        if (m_values.find(key) == m_values.end())
-        {
-            if (m_values.size() == m_maxCapacity)
-            {
-                int keyToDelete = m_countKeyMap[m_lowestFrequency].back(); 
-                m_countKeyMap[m_lowestFrequency].pop_back();
-                if (m_countKeyMap[m_lowestFrequency].empty())
-                {
-                    m_countKeyMap.erase(m_lowestFrequency);
-                }
-                m_values.erase(keyToDelete);
-                m_counts.erase(keyToDelete);
-            }
-            m_values[key].value = value;
-            m_counts[key] = 0;
-            m_lowestFrequency = 0;
-            m_countKeyMap[m_counts[key]].push_front(key);
-            m_values[key].itr = m_countKeyMap[0].begin();
+        if(keyNode.find(key) != keyNode.end()) {
+            Node* node = keyNode[key]; 
+            node->value = value; 
+            updateFreqListMap(node); 
         }
-        else
-        {
-            m_countKeyMap[m_counts[key]].erase(m_values[key].itr);
-            if (m_countKeyMap[m_counts[key]].empty())
-            {
-                if (m_lowestFrequency == m_counts[key])
-                    m_lowestFrequency++;
-                m_countKeyMap.erase(m_counts[key]);
+        else {
+            if(curSize == maxSizeCache) {
+                List* list = freqListMap[minFreq]; 
+                keyNode.erase(list->tail->prev->key); 
+                freqListMap[minFreq]->removeNode(list->tail->prev);
+                curSize--; 
             }
-            m_values[key].value = value;
-            m_counts[key]++;
-            m_countKeyMap[m_counts[key]].push_front(key);
-            m_values[key].itr = m_countKeyMap[m_counts[key]].begin();
+            curSize++;  
+            minFreq = 1; 
+            List* listFreq = new List(); 
+            if(freqListMap.find(minFreq) != freqListMap.end()) {
+                listFreq = freqListMap[minFreq]; 
+            }
+            Node* node = new Node(key, value); 
+            listFreq->addFront(node);
+            keyNode[key] = node; 
+            freqListMap[minFreq] = listFreq; 
         }
     }
 };
