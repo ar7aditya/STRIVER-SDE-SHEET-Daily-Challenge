0447bb8818f43cceaca4f4e0a5fbdec9632ebf29
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-02-04 12:31:25 +0530

    Time: 25 ms (40.66%), Space: 50.8 MB (8.73%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    string convert(string s, int nRows) {
-    
-    if (nRows <= 1)
-        return s;
-
-    const int len = (int)s.length();
-    string *str = new string[nRows];
-
-    int row = 0, step = 1;
-    for (int i = 0; i < len; ++i)
-    {
-        str[row].push_back(s[i]);
-
-        if (row == 0)
-            step = 1;
-        else if (row == nRows - 1)
-            step = -1;
-
-        row += step;
+    string convert(string s, int n) {
+        if(n==1) return s;
+        vector<string> ans(n);
+        bool is_down=true;
+        int row=0;
+        for(int i=0;i<s.size();i++){
+            ans[row].push_back(s[i]);
+            if(is_down) row++;
+            else row--;
+            if(row==n) {
+                row=n-2;
+                is_down=false;
+            }
+            if(row==-1){
+                row=1;
+                is_down=true;
+            }
+        }
+        string res="";
+        for(int i=0;i<n;i++){
+            res= res + ans[i];
+        }
+        return res;
     }
-
-    s.clear();
-    for (int j = 0; j < nRows; ++j)
-    {
-        s.append(str[j]);
-    }
-
-    delete[] str;
-    return s;
-}
 };
