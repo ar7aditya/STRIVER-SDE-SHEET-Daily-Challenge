8c6fe312922978e1bd4421a4747a27ca3454888b
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-02-06 09:49:15 +0530

    Time: 7 ms (62.32%), Space: 9.8 MB (61.93%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -3,12 +3,12 @@ public:
     vector<int> shuffle(vector<int>& nums, int n) {
         
         for(int i=n;i<nums.size();i++){
-            nums[i] = nums[i]  * 1024 + nums[i-n];
+            nums[i] = nums[i]  * 1001 + nums[i-n];
         }
         int index = 0;
         for(int i=n ; i<nums.size();i++,index+=2){
-            nums[index] = nums[i] % 1024;
-            nums[index+1] = nums[i] / 1024 ;
+            nums[index] = nums[i] % 1001;
+            nums[index+1] = nums[i] / 1001 ;
         }
         return nums;
     }
