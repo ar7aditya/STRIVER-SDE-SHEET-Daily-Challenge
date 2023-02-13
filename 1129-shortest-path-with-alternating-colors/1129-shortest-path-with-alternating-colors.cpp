bf5f2419b1895d442ab53ac539c64a24665c62a7
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-02-13 18:39:29 +0530

    Time: 21 ms (63.87%), Space: 16.2 MB (13.93%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& red_edges, vector<vector<int>>& blue_edges) {
-        // constructing adjacency matrix
 		vector<vector<pair<int,int>>> graph(n);
         for(auto edge: red_edges)
-            graph[edge[0]].emplace_back(edge[1],0); //red edges are denoted by 0 
+            graph[edge[0]].emplace_back(edge[1],0);  
         for(auto edge: blue_edges)
-            graph[edge[0]].emplace_back(edge[1],1); // blue edges are denoted by 1
+            graph[edge[0]].emplace_back(edge[1],1); 
         vector<int> dist(n,-1); 
         
-        queue<vector<int>> q;
-        q.emplace(vector<int>{0,0,-1});
+     queue<vector<int>> q;
+         q.emplace(vector<int>{0,0,-1});
         
-        while(!q.empty()) {
-            auto front = q.front();
+        while(!q.empty()){
+            vector<int> cur = q.front();
             q.pop();
-            dist[front[0]] = dist[front[0]] != -1 ? dist[front[0]] : front[1];
+            int node = cur[0];
+            int step = cur[1];
+            int prevCol = cur[2];
+            dist[node] = dist[node] != -1 ? dist[node] : step;
             
-            for(auto &adj : graph[front[0]]) {
-				//Push the node to the queue only if the next edge color is different from the pervious edge color and also if we are visiting the edge
-				//for the first time.
-                if(front[2] != adj.second && adj.first!= -1) {
-                    q.emplace(vector<int>{adj.first, front[1] + 1, adj.second});
-					//Update the value in the adjacency matrix to -1 to denote that the node has already been visited.
+            for(auto &adj : graph[node]) {
+                int nextNode = adj.first;
+                int color =  adj.second;
+                if(prevCol != color && nextNode != -1) {
+                    q.emplace(vector<int>{nextNode, step + 1, color});
                     adj.first = -1;
                 }
             }
         }
+        
         return dist;
     }
 };
