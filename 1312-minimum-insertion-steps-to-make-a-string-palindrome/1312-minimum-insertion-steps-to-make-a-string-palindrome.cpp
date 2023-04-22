f30bd3b38bdc4ef9797be0191fdb3c8bb027f615
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-04-22 23:54:34 +0530

    Time: 42 ms (82.45%), Space: 28.4 MB (50.59%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    int f(int i,int j,string& s,vector<vector<int>>& dp){
-        if(i==j) return 0;
-        if(i+1==j){
-            if(s[i]==s[j]) return 0;
-            return 1;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==s[j]) return dp[i][j] =  f(i+1,j-1,s,dp);
-        return dp[i][j] = 1 + min(f(i+1,j,s,dp),f(i,j-1,s,dp));
-    }
+    
     int minInsertions(string s) {
        int n = s.size();
-        vector<vector<int>> dp(n,vector<int> (n,-1));
-       return f(0,n-1,s,dp);
+        vector<vector<int>> dp(n,vector<int> (n,0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(s[i]==s[j]) dp[i][j] =  dp[i+1][j-1];
+                else dp[i][j] = 1 + min(dp[i+1][j],dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1];
     }
 };
