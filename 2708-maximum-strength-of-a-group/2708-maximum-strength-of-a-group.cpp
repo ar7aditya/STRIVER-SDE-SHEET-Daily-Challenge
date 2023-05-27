be0df002712332500a54b74e09f50cb7aa6c4c95
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-05-27 22:59:10 +0530

    Time: 54 ms (12.50%), Space: 46.6 MB (50.00%) - LeetHub

--- 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.cpp
+++ 2708-maximum-strength-of-a-group/2708-maximum-strength-of-a-group.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
+    long long ans = 0;
+    void f(int i,long long cur_st,int n,vector<int>& nums){
+        if(i==n){
+            ans = max(ans , cur_st);
+            return;
+        }
+        f(i+1,cur_st,n,nums); f(i+1,cur_st*nums[i],n,nums);
+        return ;
+    }
     long long maxStrength(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        long long ans = 1;
         int n = nums.size();
         if(n==1) return nums[0];
-        int i = 0;
-        while(i<n && nums[i]<0) i++;
-        int neg = i;
-        if(nums[n-1]==0 && neg==1) return 0;
+        if(nums[0] < 0 && nums[1]==0 && nums[n-1]==0) return 0;
         if(nums[0]==0 && nums[n-1]==0) return 0;
-        if(neg%2==0){
-            int k = 0;
-            while(k<neg){
-                ans = ans * nums[k];
-                k++;
-            }
-        }
-        else{
-            int k = 0;
-            while(k<neg-1){
-                ans = ans * nums[k];
-                k++;
-            }
-        }
-
-        while(i<n && nums[i]==0) i++;
-
-        int pos = i;
-        while(pos < n){
-            ans = ans * nums[pos];
-            pos++;
-        }
-            return ans;
+        f(0,1,n,nums);
+        return ans;
     }
 };
