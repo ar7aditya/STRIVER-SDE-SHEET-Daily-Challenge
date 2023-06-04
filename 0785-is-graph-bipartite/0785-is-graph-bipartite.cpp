eae8520c34429915fcb28e722c54efc27145a961
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-06-04 22:20:15 +0530

    Time: 71 ms (5.05%), Space: 17.3 MB (5.06%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-    bool bfs(int i,vector<int>& color,vector<vector<int>>& graph){
-        queue<int> q;
-        q.push(i);
-        color[i] = 0;
-        while(!q.empty()){
-            int node = q.front();
-            int col = color[node];
-            q.pop();
-            for(int i : graph[node]){
-                if(color[i]!=-1){
-                    if(color[i] == col) return false;
-                }
-                else {
-                    color[i] = 1 - col;
-                    q.push(i);
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> adj(n);
+        for(int i=0;i<graph.size();i++){
+            for(int j=0;j<graph[i].size();j++) {
+                adj[i].push_back(graph[i][j]);
+            }
+        }
+        vector<int> color(n);
+        vector<int> vis(n);
+        queue<pair<int,int>> q;   // node color
+        
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                q.push({i,0});
+                while(!q.empty()){
+                    int node = q.front().first;
+                    int col = q.front().second;
+                    vis[node] = 1;
+                    color[node] = col;
+                    q.pop();
+                    for(int next : adj[node]){
+                        if(vis[next] == 1){
+                            if(color[next] == col) return false;
+                        }
+                        else{
+                            q.push({next , 1 - col});
+                        }
+                    }
                 }
             }
         }
-        return true;
-    }
-    
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> color(n,-1);
-
-        for(int i=0;i<graph.size();i++){
-           if(color[i]==-1){
-               if(!bfs(i,color,graph)) return false;
-           }
-        }   
         return true;
     }
 };
