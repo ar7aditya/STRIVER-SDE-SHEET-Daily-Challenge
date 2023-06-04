26a8329e90cb136630d5ab5310bf8ff520f70a0c
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-06-04 13:32:01 +0530

    Time: 7 ms (57.80%), Space: 8.7 MB (79.91%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    unordered_map<Node* , Node*> mp;
-    
     Node* cloneGraph(Node* node) {
-        if(node == NULL)  return NULL;
-          if(node->neighbors.size()==0) return new Node(1);
-        
-      queue<Node*> q;
-        Node* ans=new Node(node->val);
-        mp[node]=ans;
+        queue<Node*> q;
+        if(node == NULL) return NULL;
+        if(node->neighbors.size() == 0) return new Node(node->val);
+        unordered_map<Node*,Node*> mp;
+        mp[node] = new Node(node->val);
         q.push(node);
         while(!q.empty()){
-            Node* cur=q.front();q.pop();
-            for(auto i: cur->neighbors){
-                if(mp.find(i)==mp.end()){
-                    mp[i]=new Node(i->val);
+            Node* cur = q.front();
+            q.pop();
+            for(auto i : cur->neighbors){
+                if(mp.find(i) == mp.end()){
+                    mp[i] = new Node(i->val);
                     q.push(i);
                 }
                 mp[cur]->neighbors.push_back(mp[i]);
             }
         }
-      return mp[node];
+        return mp[node];
     }
 };
