d9ff025d95fd5ba707505a04a68307b353d72816
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-06-11 12:12:17 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (23.21%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
-    int strStr(string s, string p) {
-        int i = 0;
-        int j;
-        int n = s.size();
-        while(i < n){
-            if(s[i] == p[0]){
-                j = i;
-                int k = 0;
-                while(k < n && k < p.size()){
-                    if(s[j] == p[k]) {
-                        k++;
-                        j++;
-                    }
-                    else {
-                        break;
-                    }
-                }
-                if(k == p.size()) return i;
+    vector<int> kmpProcess(string needle) {
+        int n = needle.size();
+        vector<int> lps(n, 0);
+        for (int i = 1, len = 0; i < n;) {
+            if (needle[i] == needle[len]) {
+                lps[i++] = ++len;
+            } else if (len) {
+                len = lps[len - 1];
+            } else {
+                lps[i++] = 0;
             }
-            i++;
+        }
+        return lps;
+    }
+    
+    int strStr(string s, string p) {
+        if(p.size() == 0) return 0;
+        vector<int> lps = kmpProcess(p);
+        int j = 0;
+        int i = 0;
+        while(i < s.size()){
+            if(s[i] == p[j]){
+                i++;
+                j++;
+            }
+            if(j == p.size()) return i - j;
+            if(i < s.size() && s[i] != p[j]){
+                j ? j = lps[j-1] : i++;
+            }
         }
         return -1;
     }
