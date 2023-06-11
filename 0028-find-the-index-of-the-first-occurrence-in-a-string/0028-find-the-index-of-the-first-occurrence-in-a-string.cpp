d3360cfaf588bb685f05e8b7073c19fc0dc5200e
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-06-11 09:19:18 +0530

    Time: 4 ms (39.06%), Space: 6.3 MB (61.23%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int m = haystack.size(), n = needle.size();
-        if (!n) {
-            return 0;
-        }
-        vector<int> lps = kmpProcess(needle);
-        for (int i = 0, j = 0; i < m;) {
-            if (haystack[i] == needle[j]) { 
-                i++, j++;
+    int strStr(string s, string p) {
+        int i = 0;
+        int j;
+        int n = s.size();
+        while(i < n){
+            if(s[i] == p[0]){
+                j = i;
+                int k = 0;
+                while(k < n && k < p.size()){
+                    if(s[j] == p[k]) {
+                        k++;
+                        j++;
+                    }
+                    else {
+                        break;
+                    }
+                }
+                if(k == p.size()) return i;
             }
-            if (j == n) {
-                return i - j;
-            }
-            if (i < m && haystack[i] != needle[j]) {
-                j ? j = lps[j - 1] : i++;
-            }
+            i++;
         }
         return -1;
-    }
-private:
-    vector<int> kmpProcess(string needle) {
-        int n = needle.size();
-        vector<int> lps(n, 0);
-        for (int i = 1, len = 0; i < n;) {
-            if (needle[i] == needle[len]) {
-                lps[i++] = ++len;
-            } else if (len) {
-                len = lps[len - 1];
-            } else {
-                lps[i++] = 0;
-            }
-        }
-        return lps;
     }
 };
