5502dc8c5daa1b710ff6661f13a5fc5632f3ecfa
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-06-14 17:04:55 +0530

    Time: 16 ms (50.24%), Space: 8 MB (43.69%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    int romanToInt(string s) {
-     map<char,int> mp;
- mp['I']=1; mp['V']=5; mp['X']=10; mp['L']=50; mp['C']=100; mp['D']=500; mp['M']=1000;
-        int n=s.size();
-        int ans=mp[s[n-1]];
-        int prev=mp[s[n-1]];
-        for(int i=n-2;i>=0;i--){
-            if(mp[s[i]]<prev) ans=ans-mp[s[i]];
-            else ans+=mp[s[i]];
-            prev=mp[s[i]];
+    int romanToInt(string s){
+        int ans = 0;
+        int n = s.size();
+        unordered_map<char,int> mp;
+        mp['I']=1; mp['V']=5; mp['X']=10; mp['L']=50; 
+        mp['C']=100; mp['D']=500; mp['M']=1000;
+        
+        for(int i=0;i<n-1;i++){
+            // cout<<ans<<" ";
+            if(mp[s[i]] >= mp[s[i+1]]){
+                ans += mp[s[i]];
+            }
+            else{
+                ans -= mp[s[i]];
+            }
         }
+        ans += mp[s[n-1]];
         return ans;
     }
 };
