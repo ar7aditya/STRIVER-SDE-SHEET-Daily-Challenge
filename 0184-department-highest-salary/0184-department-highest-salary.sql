7274edb6341fee0ced518d8c27c1802614bdc6e4
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-06-21 22:26:39 +0530

    Time: 2002 ms (5.93%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,7 +1,5 @@
-select d.name as Department , e.name as Employee , e.salary as Salary 
-from Employee e
-JOIN Department d
-ON e.departmentId = d.id 
-where 
-1 > ( select count(distinct salary) from Employee e2 where e2.salary > e.salary and e2.departmentId = d.id)
+SELECT dep.Name as Department, emp.Name as Employee, emp.Salary 
+from Department dep, Employee emp 
+where emp.DepartmentId=dep.Id 
+and emp.Salary=(Select max(Salary) from Employee e2 where e2.DepartmentId=dep.Id)
 
