183a12f1360fd41c30590b52d994bf625d29eb67
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-06-24 16:27:58 +0530

    Time: 207 ms (87.06%), Space: 115.7 MB (33.93%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.cpp
+++ 2742-painting-the-walls/2742-painting-the-walls.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int f(int i,vector<int>& cost,vector<int>& time ,int walls, vector<vector<int>>& dp){
+    int f(int i,int walls,vector<int>& cost,vector<int>& time, vector<vector<int>>& dp){
         if(walls <= 0) return 0;
-        if(i >= cost.size()) return 1e9;
+        if(i < 0 ) return 1e9;
         if(dp[i][walls] != -1) return dp[i][walls];
-        int pick = cost[i] + f(i+1,cost,time,walls - time[i] - 1,dp); 
-        int not_pick = 0 + f(i+1,cost,time,walls,dp);
+        int pick = cost[i] + f(i-1,walls - time[i] - 1,cost,time,dp); 
+        int not_pick = 0 + f(i-1,walls,cost,time,dp);
         return dp[i][walls] = min(pick ,not_pick );
     }
     
     int paintWalls(vector<int>& cost, vector<int>& time) {
         int n = cost.size();
         vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
-        return f(0,cost,time,n,dp);  
+        return f(n-1,n,cost,time,dp);  
     }
 };
