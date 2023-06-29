57b41ccb9f579d7e82e7f4498e454060cb2a7936
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-06-29 13:42:27 +0530

    Time: 112 ms (47.09%), Space: 38.2 MB (30.03%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,38 +1,40 @@
-class Solution {
+class Solution{ 
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<vector<int>> ans;
-         set<vector<int>> v;
-        if(n<4) return ans;
+
+    vector<vector<int> > fourSum(vector<int> &num, int target){
+        vector<vector<int> > vvi;
+        long long n = num.size();
+        if(n < 4) return  vvi;
         
-        sort(nums.begin(),nums.end());
+        sort(num.begin(), num.end());        
+        unordered_map<long long, vector<pair<long long, long long>> > mp;
+        for(long long i = 0; i < n; i++){
+            for(long long j = i + 1; j < n; j++){
+                mp[num[i]+num[j]].push_back(make_pair(i,j));
+            }
+        }
         
-        long long sum=0; 
-        for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            long long starget= (long long)target - (long long)nums[i] - (long long)nums[j];
-            int l=j+1,r=n-1;
-            while(l<r){
-                sum = (long long)nums[l] + (long long)nums[r];
-                if(sum==starget){
-                     v.insert({nums[i],nums[j],nums[l],nums[r]});
-                    l++;
-                    r--;
-                }
-                else if(sum<starget){
-                    l++;
-                }
-                else{
-                    r--;
+        for(int i = 0; i < n; i++){
+            if(i>0 && num[i] == num[i-1]) continue;
+            for(int j = i + 1; j < n; j++){
+                if(j > i + 1 && num[j] == num[j-1]) continue;
+                long long res = (long long)target - (long long)num[i] - (long long)num[j];
+                if(mp.count(res)){
+                    for(auto it = mp[res].begin(); it != mp[res].end(); it++){
+                        long long k = (*it).first, l = (*it).second;
+                        if(k > j){ 
+                            if(!vvi.empty() && num[i]==vvi.back()[0] && num[j]==vvi.back()[1]
+                            && num[k]==vvi.back()[2] && num[l] == vvi.back()[3]){
+                                continue; 
+                            }
+                            vector<int> vi={num[i], num[j], num[k], num[l]};
+                            vvi.push_back(vi);
+                        } 
+                        
+                    }
                 }
             }
         }
-        }
-        for(auto m:v){
-                ans.push_back(m);
-            }
-        return ans;
-    }  
-    
-};
+        return vvi;
+    }
+  };
