dfdaf06b1393cab726e124155b2e021b53ee6642
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-06-30 23:15:26 +0530

    Time: 47 ms (19.74%), Space: 10.7 MB (35.65%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
-     int lengthOfLongestSubstring(string s) {
-       vector<int> v(256,-1);
-       int ans=0,right=0,left=0;
-         while(right<s.size()){
-             if(v[s[right]]!=-1) left=max(left,v[s[right]]+1);
-             v[s[right]]=right;
-             ans=max(ans,right-left+1);
-             right++;
-         }
-         return ans;
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char,int> mp;
+        int cur_ans = 0;
+        int i = 0;
+        while(i < s.size() && mp.find(s[i]) == mp.end()){
+            mp[s[i]]++;
+            cur_ans++;
+            i++;
+        }
+        int ans = cur_ans;
+        int j = i;
+        i = 0;
+        for(; j < s.size(); j++){
+            char x = s[j];
+            if(mp.find(x) != mp.end()){
+                while(i < s.size() && mp.find(x) != mp.end()){
+                    mp[s[i]]--;
+                    mp.erase(s[i]);
+                    i++;
+                    cur_ans--;
+                }
+            }
+            cur_ans++;
+            mp[x]++;
+            ans = max(ans , cur_ans);
+        }
+        return ans;
     }
 };
