ae65e72b250135e7fd158c330987042aa35cf3ae
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-07-02 20:13:35 +0530

    Time: 162 ms (5.17%), Space: 77.9 MB (5.22%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-       int ans = INT_MIN;
-        int cur_sum = 0;
-        for(int i = 0; i < nums.size(); i++){
-            cur_sum += nums[i];
-            ans = max(ans , cur_sum);
-            if(cur_sum < 0) cur_sum = 0;
-        }
-        return ans;
+    int dp[100001][2];
+    int maxSubArray(vector<int>& nums) { 
+        memset(dp,-1,sizeof(dp));
+        return solve(nums, 0, false);
+    }
+    int solve(vector<int>& A, int i, bool mustPick) {
+        if(i >= size(A)) return mustPick ? 0 : -1e5;       
+        if(dp[i][mustPick] != -1) return dp[i][mustPick];
+        if(mustPick)
+            return dp[i][mustPick] = max(0, A[i] + solve(A, i+1, true));   
+        return dp[i][mustPick] = max(solve(A, i+1, false), A[i] + solve(A, i+1, true));   
     }
 };
