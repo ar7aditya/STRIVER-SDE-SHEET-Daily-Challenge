4013fc3445394d835d58fd1ee3dc9bc0d211dbc8
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-07-03 14:38:10 +0530

    Time: 3 ms (95.63%), Space: 8.3 MB (59.94%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -1,24 +1,18 @@
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-      if(head==NULL || head->next==NULL) return head;
-        ListNode* ptr1=head;
-      if(head->next->next==NULL) {
-          ptr1=head->next;
-          ptr1->next=head;
-          head->next=NULL;
-          return ptr1;
-      }
-        ptr1=head->next;
-      ListNode* ptr2=head->next->next;
-        head->next=NULL;
-    while(ptr2!=NULL){
-        ptr1->next=head;
-        head=ptr1;
-        ptr1=ptr2;
-        ptr2=ptr2->next;
-    }
-        ptr1->next=head;
-        return ptr1;
+        ListNode* temp = head;
+        if(head == NULL || head->next == NULL) return head;
+        temp = head->next;
+        ListNode* nxtPtr = temp->next;
+        head->next = NULL;
+        while(temp != NULL){
+            temp->next = head;
+            head = temp;
+            temp = nxtPtr;
+            if(temp) nxtPtr = temp->next;
+        }
+        return head;
     }
 };
