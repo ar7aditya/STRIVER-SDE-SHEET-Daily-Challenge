aa4338b3fafda20e02f83da9e426afe3851f83e7
Aditya Rana <81512209+ar7aditya@users.noreply.github.com>
2023-07-05 12:18:22 +0530

    Time: 79 ms (14.89%), Space: 21.1 MB (6.40%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *a, ListNode *b) {
-        ListNode* p1=a, *p2=b;
-        int c1=0,c2=0;
-        while(p1){
-            c1++;
-            p1=p1->next;
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_map<ListNode*,int> mp;
+        ListNode* ptr = headA;
+        while(ptr){
+            mp[ptr]++;
+            ptr = ptr->next;
         }
-         while(p2){
-            c2++;
-            p2=p2->next;
+        ptr = headB;
+        while(ptr){
+            mp[ptr]++;
+            ptr = ptr->next;
         }
-        p1=a;p2=b;
-        if(c1>c2){
-            c1=c1-c2;
-            while(c1){
-                  p1=p1->next;
-                c1--;                            
-            }
+        ptr = headA;
+        while(ptr){
+            if(mp[ptr] == 2){
+                return ptr;
+            };
+            ptr = ptr->next;
         }
-        else{
-            c2=c2-c1;
-            while(c2){
-                p2=p2->next;
-                c2--;
-            }
-        }
-        while(p1->next!=NULL && p2->next!=NULL){
-            if(p1==p2) return p1;
-            p1=p1->next;
-            p2=p2->next;
-        }
-        if(p1==p2) return p1;
         return NULL;
     }
 };
